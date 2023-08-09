38079c6fb2897035c5216ae3ced2d3dc0e5e8ebe
Pasha Musaiev <pasha.musaiev@gameloft.com>
2023-08-09 10:33:07 +0300

    Created behaviour tree, black board and behaviour script

---
+++ Source/TPD_Template/AI/ZombieCharacter.cpp
@@ -0,0 +1,23 @@
+
+
+#include "ZombieCharacter.h"
+
+AZombieCharacter::AZombieCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AZombieCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AZombieCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
---
+++ Source/TPD_Template/AI/ZombieCharacter.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ZombieCharacter.generated.h"
+
+UCLASS()
+class TPD_TEMPLATE_API AZombieCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AZombieCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+};
