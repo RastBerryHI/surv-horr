56f0500f518cc53406c00b5d7e0615e1c72956ee
Pasha Musaiev <pasha.musaiev@gameloft.com>
2023-08-09 15:38:02 +0300

    Added AI and Task modules. Added BTTask

---
+++ Source/TPD_Template/AI/BTTaskNode_FindRandomLocation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTaskNode_FindRandomLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPD_TEMPLATE_API UBTTaskNode_FindRandomLocation : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
